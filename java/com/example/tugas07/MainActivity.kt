3eb7c3503ee4e9b1393d8bb181b5a673a62192ad
Diah Fatmawati <61172685+diahfatmawati@users.noreply.github.com>
2020-11-22 09:10:33 +0700

    Tugas Modul 7 Pemrograman Mobile
    Diah Fatmawati
    18082010012

---
+++ java/com/example/tugas07/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.tugas07
+
+import com.example.tugas07.databinding.ActivityMainBinding
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = DataBindingUtil
+            .setContentView<ActivityMainBinding>(
+                this,
+                R.layout.activity_main
+            )
+        drawerLayout = binding.drawerLayout
+        val navController =
+            this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(
+            binding
+                .navView, navController
+        )
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return NavigationUI.navigateUp(navController, drawerLayout)
+    }
+}
